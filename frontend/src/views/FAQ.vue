d884daf002f64a418f759173a5cbaeb3120024b9
brschkv <brschkv@protonmail.com>
2020-10-06 14:36:52 +0200

    Frontend Text

--- frontend/src/views/FAQ.vue
+++ frontend/src/views/FAQ.vue
@@ -6,6 +6,7 @@
         by doing a bayesian estimation based on past price developments. For a more detailed discussion
         please refer to <a href="https://github.com/brschkv/yearn-roi-model/blob/master/DISCUSSION.md"> this
           discussion</a>.
+        The model estimates prices of the next n days ("Horizon") based on the prices of the last m ("Based on last") days.
         The short of it is this: According to the model(!!) there is a 50% chance that the future price at
         a given date is within the blue area (This is called a 'HPD' in bayesian terms, hence the label in the graph).
         And, according to the model(!!) there is a 95% chance that the future price is within the grey area.
